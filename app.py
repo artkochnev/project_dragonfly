4d364cbe0c149dfdddff1cdbde3b94737de8b732
artkochnev <100959553+artkochnev@users.noreply.github.com>
2022-10-16 20:53:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ year_list = df_main["YEAR"].unique()
 st.title("CLIMATE REVENUE DISCOUNT FACTORS")
 st.write("Factors show the annual increase of company costs measured as percent of industry revenues driven by the carbon price increase. Calculated according to the formula below")
 st.latex(r'''
-    \delta = \frac{\text{CO2 Emissions(tn)} \times \text{ETS Price(USD/tn)}}{\text{Industry Output, USD}} \times \text{Tax rate}
+    \delta = \frac{\text{Industry CO2 Emissions(tn)} \times \text{ETS Price(USD/tn)}}{\text{Industry Output, USD}} \times \text{Tax rate}
     ''')
 st.write("Tax rate assumed to be 10%, which is the carbon price increase compatible with the 1.5 degree goal")
 
